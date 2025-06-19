fbbd584bb1e68d78377cdadb23b9e2bee61a1e44
William Fernandes Dias <william.winchester1967@gmail.com>
2025-06-18 23:30:38 -0300

    build: update dependencies and tools versions

--- tools.go
+++ tools.go
@@ -5,7 +5,5 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/securego/gosec/v2/cmd/"
-	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
 )
